190afb723d63c7e5f47295ee2106df50872c5869
Rivaldo <Rivaldo20@mahasiswa.pcr.ac.id>
2021-07-14 10:32:57 +0700

    Dah fix

--- app/Views/user/index.php
+++ app/Views/user/index.php
@@ -47,6 +47,7 @@
       <!-- <a href="index.html" class="logo me-auto me-lg-0"><img src="<?php echo base_url() ?>/assets/user/assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
       <nav id="navbar" class="navbar order-last order-lg-0">
+      <h1></h1>
         <ul>
           <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
           <li><a class="nav-link scrollto" href="#about">About</a></li>
