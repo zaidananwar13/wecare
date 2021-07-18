a6e4c7d0a08de243eaf90df903eaf4b0b4e64bcc
zaidananwar13 <zaidan20ti@mahasiswa.pcr.ac.id>
2021-07-18 10:47:15 +0700

    Commited

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -72,7 +72,6 @@ class Auth extends BaseController
       try {
 
         $plus = new \Google_Service_OAuth2($client);
-        var_dump($plus->userinfo->get()); die;
         $_SESSION['access_profile'] = $plus->userinfo->get();
 
       }catch(\Exception $e) {
